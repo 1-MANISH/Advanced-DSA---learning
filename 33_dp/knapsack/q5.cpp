a5ff72826b01fb93a3a86fad03cc6bf02e1337d1
manish_patidar <shanimpatidar@gmail.com>
2026-09-11 22:25:46 +0530

    Solved more problem on dp

--- 33_dp/knapsack/q5.cpp
+++ 33_dp/knapsack/q5.cpp
@@ -1,13 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// E. Partition Into Equal Sum
+// https://codeforces.com/group/4vcXCPx8NY/contest/715177/problem/E
+const int N = 1e2;
+const int M = 1e5;
+vector<vector<int>>dp(N+1,vector<int>(M+1,-1));
+
+bool isEqualSum(int index,int currentSum,int &totalSum,int &n,vector<int>&arr){
+
+    if(index>=n){
+        return currentSum==totalSum-currentSum;
+    }
+
+    if(dp[index][currentSum]!=-1){
+        return dp[index][currentSum];
+    }
+
+    // take it
+    bool a1 = isEqualSum(index+1,currentSum+arr[index],totalSum,n,arr);
+
+
+    // not take it
+    bool a2 = isEqualSum(index+1,currentSum,totalSum,n,arr);
+
+    return dp[index][currentSum]= a1 or a2;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n,capacity;
-    cin >> n >> capacity;
+    int n;
+    cin >> n ;
 
+    vector<int>arr(n);
+    int totalSum = 0;
+    for(int i = 0  ; i < n ; i++){
+        cin >> arr[i];
+        totalSum+=arr[i];
+    }
+
+    cout << (isEqualSum(0,0,totalSum,n,arr)?"YES\n":"NO\n");
 
     return 0;
 
--- 33_dp/knapsack/q6.cpp
+++ 33_dp/knapsack/q6.cpp
@@ -1,14 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int M = 1e9+7;
+const int N = 1e3;
+const int S = 1e3;
+vector<vector<int>>dp(N+1,vector<int>(S+1,-1));
+
+int countSubsetSum(int index,int currentSum,int &target,int &n,vector<int>&arr){
+
+    if(index>=n){
+        return currentSum==target ? 1 : 0;
+    }
+
+    if(currentSum>target){
+        return  0;// as all number are positive only
+    }
+
+    if(dp[index][currentSum]!=-1){
+        return dp[index][currentSum];
+    }
+
+    // take it
+    int a1 = countSubsetSum(index+1,currentSum+arr[index],target,n,arr);
+
+    // not take it
+    int a2 = countSubsetSum(index+1,currentSum,target,n,arr);
+
+
+    return dp[index][currentSum] =  (a1 %M + a2 % M )%M;
+}
+
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n,capacity;
-    cin >> n >> capacity;
+    int n,target;
+    cin >> n >> target;
+    vector<int>arr(n);
 
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+
+    cout << countSubsetSum(0,0,target,n,arr);
 
     return 0;
-
 }
